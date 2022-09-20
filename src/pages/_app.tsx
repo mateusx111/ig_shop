d698610827c4e56cf731b579b592ad480a241aa9
mateus santos <msouza954@gmail.com>
2022-09-20 10:44:42 -0300

    routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,21 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app"
+
+globalStyles() // estilo global que sera carregado para todas as paginas
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
-export default MyApp
+
